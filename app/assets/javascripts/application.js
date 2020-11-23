a16c567c325f5a3c0850205c8acc7470debd50b1
vagrant <vagrant@localhost.localdomain>
2020-11-23 19:01:15 +0000

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,71 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+//= require jquery
+//= require bootstrap-sprockets
+//= require moment
+//= require fullcalendar
+
+$(function () {
+
+    $(document).on('turbolinks:load', function () {
+        if ($('#calendar').length) {
+
+            function Calendar() {
+                return $('#calendar').fullCalendar({
+                });
+            }
+            function clearCalendar() {
+                $('#calendar').html('');
+            }
+
+            $(document).on('turbolinks:load', function () {
+                Calendar();
+            });
+            $(document).on('turbolinks:before-cache', clearCalendar);
+
+            $('#calendar').fullCalendar({
+                events: '/events.json',
+                titleFormat: 'YYYY年 M月',
+                dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
+                header: {
+                    left: '',
+                    center: 'title',
+                    right: 'today prev,next'
+                },
+                defaultTimedEventDuration: '03:00:00',
+                buttonText: {
+                    prev: '前',
+                    next: '次',
+                    prevYear: '前年',
+                    nextYear: '翌年',
+                    today: '今日',
+                    month: '月',
+                    week: '週',
+                    day: '日'
+                },
+                editable: true,
+                timeFormat: "HH:mm",
+                eventColor: '#87cefa',
+                eventTextColor: '#000000',
+                eventRender: function(event, element) {
+                    element.css("font-size", "0.8em");
+                    element.css("padding", "5px");
+                }
+            });
+        }
+    });
+});
